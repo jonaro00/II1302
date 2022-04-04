a474ac0f225b8720a89ab45f868e20731180aeb3
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-04-04 13:22:43 +0200

    Add web project

---
+++ web/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Spafe Monitor</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Spafe Monitor
+        </h1>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
