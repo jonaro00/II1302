66f1dd1d142c01bc2b395e607ac6976fe6b18528
calexanderberg <carlalexanderberg@gmail.com>
2022-04-11 15:18:11 +0200

    added sandbox folder and semantic as dependency

---
+++ web/pages/sandbox/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Home
