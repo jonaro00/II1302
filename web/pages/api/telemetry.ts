e36fccd3877f4e5c79963e62f0b22892d6c4e13b
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-04-14 18:43:13 +0200

    Azure validation of telemetry endpoint 3

--- web/pages/api/telemetry.ts
+++ web/pages/api/telemetry.ts
@@ -1,27 +1,31 @@
+import { EventGridDeserializer, EventGridEvent, SubscriptionValidationEventData } from '@azure/eventgrid'
+import { AzureLogger, setLogLevel } from '@azure/logger'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type AzureValidationResponse = {
-    validationResponse: string,
+    validationResponse: string
 }
 
-export default function handler(
+setLogLevel('verbose')
+
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<AzureValidationResponse|string>
 ) {
+    AzureLogger.log(req.body)
     if (req.method !== 'POST') {
         // Method not allowed
         res.status(405).end()
         return
     }
-    // Azure validation https://docs.microsoft.com/en-us/azure/event-grid/webhook-event-delivery#validation-details
-    if (req.headers['aeg-event-type'] === 'SubscriptionValidation') {
-        console.log(JSON.parse(req.body))
-        let validationCode: string = JSON.parse(req.body)[0].data.validationCode
-        res.status(200).json({validationResponse: validationCode})
+
+    let egd = new EventGridDeserializer()
+    let events = await egd.deserializeEventGridEvents(req.body)
+    let event = events[0] as EventGridEvent<SubscriptionValidationEventData>
+    if (event.eventType === 'Microsoft.EventGrid.SubscriptionValidationEvent') {
+        res.status(200).json({validationResponse: event.data.validationCode})
         return
     }
-    // Telemetry
-    //...
 
     // Base case
     res.status(400).end()
