ef68064761f32183a17682814099c4391d65573c
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-04-29 17:17:07 +0200

    Lint DAO.ts

--- web/integration/DAO.ts
+++ web/integration/DAO.ts
@@ -1,23 +1,22 @@
-const cls = require('cls-hooked');
+import { createNamespace } from 'cls-hooked'
 import { Sequelize, Dialect } from 'sequelize'
-// import * as fs from 'fs'
-const fs = require('fs')
+import * as fs from 'fs'
 import { Sensor } from '../model/Sensor'
 import { User } from '../model/User'
 import { Telemetry } from '../model/Telemetry'
 
 export type UserInfo = {
-  id: string,
-  name: string,
+  id: string
+  name: string
 }
 
 export class DAO {
   public database: Sequelize
 
   constructor() {
-    const ns = cls.createNamespace(process.env.DB_NAME);
+    const ns = createNamespace(process.env.DB_NAME || '')
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    Sequelize.useCLS(ns);
+    Sequelize.useCLS(ns)
     this.database = new Sequelize(
       process.env.DB_NAME || '',
       process.env.DB_USER || '',
@@ -25,17 +24,18 @@ export class DAO {
       {
         host: process.env.DB_HOST || '',
         port: Number.parseInt(process.env.DB_PORT || ''),
-        dialect: (process.env.DB_DIALECT as Dialect),
+        dialect: process.env.DB_DIALECT as Dialect,
+        // @ts-ignore
         ssl: {
-          ca: fs.readFileSync('integration/Azure-db-DigiCertGlobalRootCA.crt.pem')
-        }
-      }
-    );
+          ca: fs.readFileSync('integration/Azure-db-DigiCertGlobalRootCA.crt.pem'),
+        },
+      },
+    )
     Sensor.createModel(this.database)
     User.createModel(this.database)
     Telemetry.createModel(this.database)
     this.makeTables()
-    console.log("Contructing DAO")
+    console.log('Contructing DAO')
   }
 
   public static async createDAO(): Promise<DAO> {
@@ -45,22 +45,21 @@ export class DAO {
   private async makeTables(): Promise<void> {
     await this.database.authenticate()
     await this.database.sync({ force: false, alter: false })
-    console.log("Making Tables!")
+    console.log('Making Tables!')
   }
 
   public async login(username: string, password: string): Promise<UserInfo> {
-    console.log("Logging in!")
+    console.log('Logging in!')
     const matchingUser = await User.findOne({
-      where: { username: username }
-    });
-    if (matchingUser === null) throw new Error("No user with that name found!")
+      where: { username: username },
+    })
+    if (matchingUser === null) throw new Error('No user with that name found!')
 
     console.log(matchingUser.get('password'))
     if (await User.validPassword(password, matchingUser.get('password') as string)) {
       const { id, name } = matchingUser.get({ plain: true })
       return { id, name }
-    }
-    else throw new Error("Invalid password!")
+    } else throw new Error('Invalid password!')
   }
 
   public async register(username: string, password: string): Promise<UserInfo> {
