eeefee87b82c8cd3911750e42ffdc35f89c552cb
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-04-29 13:02:27 +0200

    Observable interface

---
+++ web/model/Observable.ts
@@ -0,0 +1,22 @@
+export class Observable {
+    private observers: Array<Function>
+
+    public constructor() {
+        this.observers = []
+    }
+    public addObserver(callback: Function): void {
+        this.observers = [...this.observers, callback]
+    }
+    public removeObserver(callback: Function): void {
+        this.observers = this.observers.filter((ob) => ob !== callback)
+    }
+    public notifyObservers(): void {
+        this.observers.forEach((cb) => {
+            try {
+                cb()
+            } catch (error) {
+                // prevent one observer error from stopping the other callbacks to happen
+            }
+        });
+    }
+}
