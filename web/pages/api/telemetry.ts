6bcec41a3caf1639c042b19d62072ebbde4288fd
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-04-29 17:18:15 +0200

    Bing Linting

--- web/pages/api/telemetry.ts
+++ web/pages/api/telemetry.ts
@@ -9,48 +9,43 @@ import { AzureLogger } from '@azure/logger'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type AzureValidationResponse = {
-    validationResponse: string
+  validationResponse: string
 }
 
 const egd = new EventGridDeserializer()
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<AzureValidationResponse|undefined>
+  req: NextApiRequest,
+  res: NextApiResponse<AzureValidationResponse | undefined>,
 ) {
-    if (req.method !== 'POST') {
-        // Method not allowed
-        res.status(405).end()
-        return
+  if (req.method !== 'POST') {
+    // Method not allowed
+    res.status(405).end()
+    return
+  }
+
+  let events = await egd.deserializeEventGridEvents(req.body)
+  events.forEach((event: any): void => {
+    AzureLogger.log(`Received Event grid event:\n${JSON.stringify(event)}`)
+    if (isSystemEvent('Microsoft.EventGrid.SubscriptionValidationEvent', event)) {
+      // Azure Webhook validation https://docs.microsoft.com/en-us/azure/event-grid/webhook-event-delivery#validation-details
+      res.status(200).json({ validationResponse: event.data.validationCode })
+      return
     }
+    if (isSystemEvent('Microsoft.Devices.DeviceCreated', event)) {
+      // event.timestamp, event.data.deviceId
+    } else if (isSystemEvent('Microsoft.Devices.DeviceDeleted', event)) {
+      // event.timestamp, event.data.deviceId
+    } else if (isSystemEvent('Microsoft.Devices.DeviceConnected', event)) {
+      // event.timestamp, event.data.deviceId
+    } else if (isSystemEvent('Microsoft.Devices.DeviceDisconnected', event)) {
+      // event.timestamp, event.data.deviceId
+    } else if (isSystemEvent('Microsoft.Devices.DeviceTelemetry', event)) {
+      // event.timestamp, event.data.deviceId, event.data.body
+    } else {
+      AzureLogger.log('Event went unhandled...')
+    }
+  })
 
-    let events = await egd.deserializeEventGridEvents(req.body)
-    events.forEach((event: any): void => {
-        AzureLogger.log(`Received Event grid event:\n${JSON.stringify(event)}`)
-        if (isSystemEvent('Microsoft.EventGrid.SubscriptionValidationEvent', event)) {
-            // Azure Webhook validation https://docs.microsoft.com/en-us/azure/event-grid/webhook-event-delivery#validation-details
-            res.status(200).json({validationResponse: event.data.validationCode})
-            return
-        }
-        if      (isSystemEvent('Microsoft.Devices.DeviceCreated', event)) {
-            // event.timestamp, event.data.deviceId
-        }
-        else if (isSystemEvent('Microsoft.Devices.DeviceDeleted', event)) {
-            // event.timestamp, event.data.deviceId
-        }
-        else if (isSystemEvent('Microsoft.Devices.DeviceConnected', event)) {
-            // event.timestamp, event.data.deviceId
-        }
-        else if (isSystemEvent('Microsoft.Devices.DeviceDisconnected', event)) {
-            // event.timestamp, event.data.deviceId
-        }
-        else if (isSystemEvent('Microsoft.Devices.DeviceTelemetry', event)) {
-            // event.timestamp, event.data.deviceId, event.data.body
-        }
-        else {
-            AzureLogger.log('Event went unhandled...')
-        }
-    })
-
-    res.status(200).end()
+  res.status(200).end()
 }
