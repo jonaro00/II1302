81f9661788bb49c46e527aabbc4235e275b20b9b
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-05-01 21:21:56 +0200

    Don't run DAO in frontend

--- web/pages/sandbox/index.tsx
+++ web/pages/sandbox/index.tsx
@@ -2,15 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
-import { DAO } from '../../integration/DAO'
 
-export async function getServerSideProps(context: any) {
-  return {
-    props: { dao: await DAO.createDAO() }
-  }
-}
-
-export default function SandBox({ dao }: { dao: DAO }) {
+export default function SandBox() {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,11 +12,8 @@ export default function SandBox({ dao }: { dao: DAO }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hit Maxes And Evade Taxes
-        </h1>
+        <h1 className={styles.title}>Hit Maxes And Evade Taxes</h1>
       </main>
-
     </div>
   )
 }
