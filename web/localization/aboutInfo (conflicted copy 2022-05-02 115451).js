3a654033b7837565bc289230ae17e690072cdd5e
calexanderberg <carlalexanderberg@gmail.com>
2022-05-02 12:26:18 +0200

    lowkey want to give up

---
+++ web/localization/aboutInfo (conflicted copy 2022-05-02 115451).js
@@ -0,0 +1,64 @@
+
+
+export const groupInfo = [
+  {
+    name: 'Alex',
+    tel: 'Lorem ipsum',
+    telTxt: 'Lorem ipsum',
+    mail: 'Lorem ipsum',
+    gitHub: 'Lorem ipsum ',
+    linkedin: 'https://linkedin.com/in/calexanderberg',
+    title: 'Lorem ipsum',
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
+    Picture: '',
+  },  
+  {
+    name: 'Johan',
+    tel: 'Lorem ipsum',
+    telTxt: 'Lorem ipsum',
+    mail: 'Lorem ipsum',
+    gitHub: 'Lorem ipsum ',
+    linkedin: 'Lorem ipsum ',
+    title: 'Lorem ipsum ',
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
+    Picture: '',
+  },
+  {
+    name: 'Bashar',
+    tel: 'Lorem ipsum',
+    telTxt: 'Lorem ipsum',
+    mail: 'Lorem ipsum',
+    gitHub: 'Lorem ipsum ',
+    linkedin: 'Lorem ipsum ',
+    title: 'Lorem ipsum ',
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
+    Picture: '',
+  },
+  {
+    name: 'Amiran',
+    tel: 'Lorem ipsum',
+    telTxt: 'Lorem ipsum',
+    mail: 'Lorem ipsum',
+    gitHub: 'Lorem ipsum ',
+    linkedin: 'Lorem ipsum ',
+    title: 'Lorem ipsum ',
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
+    Picture: '',
+  },
+  {
+    name: 'Simon',
+    tel: 'Lorem ipsum',
+    telTxt: 'Lorem ipsum',
+    mail: 'Lorem ipsum',
+    gitHub: 'Lorem ipsum',
+    linkedin: 'Lorem ipsum ',
+    title: 'Lorem ipsum ',
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
+    Picture: '',
+  },
+];
