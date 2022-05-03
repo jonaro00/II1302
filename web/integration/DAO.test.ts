8a3f3020df34d881f78867e41e239d2a580b0e79
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-05-03 02:45:30 +0200

    Combine DB tests into one

--- web/integration/DAO.test.ts
+++ web/integration/DAO.test.ts
@@ -9,34 +9,14 @@ afterEach(async () => {
   await dao.database.close()
 })
 
-test('Set up DB models', async () => {
+test('Register a user and log in', async () => {
   expect(Object.keys(dao.database.models)).toHaveLength(allDBModels.length)
-})
-
-test('Register a user', async () => {
   const testname = 'user1',
     testpass = 'password1'
   const { id, name } = await dao.register(testname, testpass)
   expect(id).not.toBeNull()
   expect(name).toEqual(testname)
-})
-
-test('Register a user twice', async () => {
-  const testname = 'user1',
-    testpass = 'password1'
-  await dao.register(testname, testpass)
   expect(dao.register(testname, testpass)).rejects.toThrow('Failed to register user.')
-})
-
-test('Login with incorrect password', async () => {
-  const testname = 'user1',
-    testpass = 'password1'
-  await dao.register(testname, testpass)
   expect(dao.login(testname, '1234')).rejects.toThrow('Invalid password!')
-})
-
-test('Login with incorrect user', async () => {
-  const testname = 'admin',
-    testpass = 'admin'
-  expect(dao.login(testname, testpass)).rejects.toThrow('No user with that name found!')
+  expect(dao.login('admin', testpass)).rejects.toThrow('No user with that name found!')
 })
