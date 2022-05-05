b092b5d1a3ca6eedb10eb2b55a4aab8a8d93627c
Bansheebomb <dugiev123@gmail.com>
2022-05-06 00:21:03 +0200

    added nextauth

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,17 +3,36 @@ import 'semantic-ui-css/semantic.min.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Model } from '../model/Model'
+import { SessionProvider, useSession } from 'next-auth/react'
 
 const model = new Model()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <Head>
         <title>Spafe Monitor</title>
         <link rel="icon" href="/favicon-16x16.png" />
       </Head>
-      <Component {...pageProps} model={model} />
+      <SessionProvider session={session}>
+        {(Component as any).auth ? (
+          <Auth>
+            <Component {...pageProps} model={model} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} model={model} />
+        )}
+      </SessionProvider>
     </>
   )
+}
+
+function Auth({ children }: { children: any }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true })
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
+  return children
 }
--- web/pages/devices/index.tsx
+++ web/pages/devices/index.tsx
@@ -1,11 +1,17 @@
 import MainLayout from '../../components/MainLayout'
 import DevicePresenter from '../../presenters/DevicePresenter'
 import { Model } from '../../model/Model'
+import { useSession } from 'next-auth/react'
 
 export default function DevicePage({ model }: { model: Model }) {
+  const { data: session } = useSession()
   return (
     <MainLayout model={model}>
-      <DevicePresenter />
+      <>
+        <p>{session?.user?.name}</p>
+        <DevicePresenter />
+      </>
     </MainLayout>
   )
 }
+DevicePage.auth = true
