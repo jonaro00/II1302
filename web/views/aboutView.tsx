76837d7ade6ab2a1b369e26ab36033fd46efcd1f
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-05-05 17:11:50 +0200

    Add profile info and fix about page

--- web/views/aboutView.tsx
+++ web/views/aboutView.tsx
@@ -1,6 +1,18 @@
 import { Container, Header, Grid, Card, Image, Icon, Item, Segment } from 'semantic-ui-react'
 
-export const groupInfo = [
+type Profile = {
+  name: string
+  tel: string
+  telTxt: string
+  mail: string
+  gitHub: string
+  linkedin: string
+  title: string
+  about: string
+  picture: string
+}
+
+export const groupInfo: Array<Profile> = [
   {
     name: 'Alex',
     tel: '+46708128516',
@@ -11,31 +23,31 @@ export const groupInfo = [
     title: 'Projektledare/Hållbarhets- och arbetsmiljöansvarig',
     about:
       'I am a Swedish Computer Science student at KTH. Besides my beaurocatic responsibilities for this  project I was responsible for the Front-end programming for our software side.',
-    Picture: '/images/alex.jpg',
+    picture: '/images/alex.jpg',
   },
   {
     name: 'Johan',
-    tel: 'Lorem ipsum',
-    telTxt: 'Lorem ipsum',
+    tel: '',
+    telTxt: '',
     mail: 'jberg8@kth.se',
-    gitHub: 'https://linkedin.com/in/jonaro00',
-    linkedin: 'Lorem ipsum ',
-    title: 'Lorem ipsum ',
+    gitHub: 'https://github.com/jonaro00',
+    linkedin: '',
+    title: 'Architect 🛠',
     about:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
-    Picture: '',
+      'I study Computer Science at KTH. As the architect in this project, I was involved in choosing most of the systems and code frameworks involved, and connecting them.',
+    picture: '/images/wallace.png',
   },
   {
     name: 'Bashar',
-    tel: '+46 729 38 68 03',
-    telTxt: 'Lorem ipsum',
+    tel: '+46729386803',
+    telTxt: '+46 729 38 68 03',
     mail: 'bjpati@kth.se',
     gitHub: 'https://github.com/bjpati',
     linkedin: 'https://www.linkedin.com/in/bashar-pati-89735b124/',
     title: 'Test ansvarig',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
-    Picture: 'https://github.com/bjpati/bjpati/blob/main/Icon/IMG_3796%202.jpeg',
+    picture: 'https://raw.githubusercontent.com/bjpati/bjpati/main/Icon/IMG_3796%202.jpeg',
   },
   {
     name: 'Amiran',
@@ -47,7 +59,7 @@ export const groupInfo = [
     title: 'Lorem ipsum ',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
-    Picture: '',
+    picture: '',
   },
   {
     name: 'Simon',
@@ -59,7 +71,7 @@ export const groupInfo = [
     title: 'Lorem ipsum ',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, felis pharetra fermentum commodo, tellus magna vehicula arcu, porta gravida est purus ut nisl. Morbi vel semper ante.',
-    Picture: '',
+    picture: '',
   },
 ]
 
@@ -68,14 +80,24 @@ const AboutView = () => {
     <Segment>
       <Container text textAlign="center">
         <p className="about-p about-p-top">
-          Spafe Monitor believes in total survelance of all our customers, whether they are aware of it ot not.
+          Spafe Monitor believes in total survelance of all our customers, whether they are aware of
+          it ot not.
         </p>
         <Icon name="eye" circular color="red" size="huge" inverted />
         <Header as="h2">Spafe Monitor</Header>
         <p className="about-p">
-          Spafe Monitor started in 2022 with the explicit goal of creating the most advanced and high tech monitoring system of temperature and co2 levels. Started as a project for the Projects and Project methods course at KTH, Spafe was created to fill the need to fast, afforable, and easy to use monitoring of everything in our lives. 
-          <br></br>Spafe is constructed into two departments each working towards reaching a specific goal. The first one being the hardware, with the explicit goal of creating small and accurate products so you don't need to even be aware that it even exists in your house. 
-          <br></br>The second department is the software, split into front-end and back-end. For the front-end or gui it works with you to make sure you get the information you need and nothing else. On the other hand, the backend works to collect all the information we need and sending it to the appropriate channels.
+          Spafe Monitor started in 2022 with the explicit goal of creating the most advanced and
+          high tech monitoring system of temperature and co2 levels. Started as a project for the
+          Projects and Project methods course at KTH, Spafe was created to fill the need to fast,
+          afforable, and easy to use monitoring of everything in our lives.
+          <br></br>Spafe is constructed into two departments each working towards reaching a
+          specific goal. The first one being the hardware, with the explicit goal of creating small
+          and accurate products so you don{"'"}t need to even be aware that it even exists in your
+          house.
+          <br></br>The second department is the software, split into front-end and back-end. The
+          front-end or GUI works with you to make sure you get the information you need and nothing
+          else. On the other hand, the backend works to collect all the information we need and
+          sending it to the appropriate channels.
           <br></br>Spafe is a company you can trust.
         </p>
       </Container>
@@ -85,7 +107,7 @@ const AboutView = () => {
             return (
               <Card className="about-card" key={p.name}>
                 <Image
-                  src={p.Picture}
+                  src={p.picture}
                   alt={'Picture of ' + p.name}
                   wrapped
                   ui={false}
@@ -98,16 +120,24 @@ const AboutView = () => {
                   <Card.Meta textAlign="center">{p.title}</Card.Meta>
                   <Card.Description>{p.about}</Card.Description>
                   <Card.Content extra className="about-contact">
-                    <Card.Meta href={'mailto:' + p.mail}>{p.mail}</Card.Meta>
-                    <Card.Meta href={'tel:+' + p.tel}>{p.telTxt}</Card.Meta>
+                    {p.mail ? <Card.Meta href={'mailto:' + p.mail}>{p.mail}</Card.Meta> : false}
+                    {p.tel ? <Card.Meta href={'tel:' + p.tel}>{p.telTxt}</Card.Meta> : false}
                   </Card.Content>
                   <Card.Content extra>
-                    <Item href={p.gitHub} target="_blank">
-                      <Icon name="github" color="black" />
-                    </Item>
-                    <Item href={p.linkedin} target="_blank">
-                      <Icon name="linkedin" />
-                    </Item>
+                    {p.gitHub ? (
+                      <Item href={p.gitHub} target="_blank">
+                        <Icon name="github" color="black" />
+                      </Item>
+                    ) : (
+                      false
+                    )}
+                    {p.linkedin ? (
+                      <Item href={p.linkedin} target="_blank">
+                        <Icon name="linkedin" />
+                      </Item>
+                    ) : (
+                      false
+                    )}
                   </Card.Content>
                 </Card.Content>
               </Card>
