e01ee7dbbd7537921cc3b65df65a5e902f6c7ea2
calexanderberg <carlalexanderberg@gmail.com>
2022-05-06 13:23:58 +0200

    changed devices and footer/home view

--- web/views/homeView.tsx
+++ web/views/homeView.tsx
@@ -8,16 +8,6 @@ const HomeView = () => {
       <p className="home-p">
         We are you all in one solution for monitoring, visualizing, and alertion for your co2, and temperature levels. 
         Our devices are state of the art, combining complex solutions in easy to read and understand interfaces. 
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-        consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu
-        pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.
-        Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend
-        ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-        nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel
-        augue. Curabitur ullamcorper ultricies nisi.
       </p>
       <p className="home-p">
         If you need help setting up your device, you can follow our documentation, you can reach
