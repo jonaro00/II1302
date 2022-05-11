6ec1b63eb44c6e6c99e2edbf49d20c9c55a30115
Bansheebomb <dugiev123@gmail.com>
2022-05-11 03:03:09 +0200

    updated model

--- web/integration/DAO.ts
+++ web/integration/DAO.ts
@@ -1,7 +1,7 @@
 import { createNamespace } from 'cls-hooked'
 import { Sequelize, Dialect } from 'sequelize'
 import fs from 'fs'
-import { Sensor } from '../model/Sensor'
+import { Sensor, SensorType } from '../model/Sensor'
 import { User, UserCredentials, UserType } from '../model/User'
 import { Telemetry } from '../model/Telemetry'
 import { Alarm } from '../model/Alarm'
@@ -103,6 +103,90 @@ export class DAO {
       return this.login({ username, password })
     } catch (error) {
       throw new Error('Failed to register user.')
+    }
+  }
+
+  public async getUserId({ username, password }: UserCredentials): Promise<number> {
+    return await this.database.transaction(async t => {
+      const matchingUser = await User.findOne({
+        where: { username },
+      })
+      if (matchingUser === null) throw new Error('No user with that name found!')
+      if (await User.validPassword(password, matchingUser.get('password') as string)) {
+        const { id } = matchingUser.get({ plain: true })
+        return id
+      } else throw new Error('Invalid password!')
+    })
+  }
+
+  public async getSensors({ username, password }: UserCredentials): Promise<SensorType[]> {
+    return await this.database.transaction(async t => {
+      var userId = await this.getUserId({ username, password })
+      const matchingSensor = await Sensor.findAll({
+        where: { userId },
+      })
+      if (matchingSensor === null) throw new Error('No sensor with that name found!')
+      let sensorArray: SensorType[] = []
+      for (let i = 0; i < Object.keys(matchingSensor).length; i++) {
+        sensorArray[i] = matchingSensor[i].get({ plain: true })
+      }
+      return sensorArray
+    })
+  }
+
+  public async addSensor({
+    id,
+    device_azure_name,
+    user_id,
+    location,
+    createdAt,
+    updatedAt,
+  }: SensorType): Promise<void> {
+    try {
+      await this.database.transaction(async t => {
+        await Sensor.create({ id, device_azure_name, user_id, location, createdAt, updatedAt })
+      })
+      return
+    } catch (error) {
+      throw new Error('Failed to add sensor.')
+    }
+  }
+
+  public async updateSensor({
+    id,
+    device_azure_name,
+    user_id,
+    location,
+    createdAt,
+    updatedAt,
+  }: SensorType): Promise<void> {
+    try {
+      await this.database.transaction(async t => {
+        const matchingSensor = await Sensor.build({ id })
+        matchingSensor.set({
+          device_azure_name: device_azure_name,
+          user_id: user_id,
+          location: location,
+          createdAt: createdAt,
+          updatedAt: updatedAt,
+        })
+        await matchingSensor.save()
+      })
+      return
+    } catch (error) {
+      throw new Error('Failed to update sensor.')
+    }
+  }
+
+  public async deleteSensor({ id }: SensorType): Promise<void> {
+    try {
+      await this.database.transaction(async t => {
+        const matchingSensor = await Sensor.build({ id })
+        await matchingSensor.destroy()
+      })
+      return
+    } catch (error) {
+      throw new Error('Failed to delete sensor.')
     }
   }
 }
