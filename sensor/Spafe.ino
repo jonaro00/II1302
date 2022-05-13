3c4fd565664000d056feba889a917f28b687e7a8
Bashar Pati <bjpati@kth.se>
2022-05-13 12:40:19 +0200

    Hardware uppdate

--- sensor/Spafe.ino
+++ sensor/Spafe.ino
@@ -1,41 +1,21 @@
-#include "Azure.cpp"
-#include "config.h"
+#include "Azure.h"
+#include "MQ2.h"
+#include "TempSensor.h"
 
 // Arduino setup and loop main functions.
 
-/*
 void setup()
 {
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   establishConnection();
-}
-
-void loop()
-{
-  if (millis() > next_telemetry_send_time_ms)
-  {
-    // Check if connected, reconnect if needed.
-    if(!mqtt_connected())
-    {
-      establishConnection();
-    }
-
-    sendTelemetry();
-    next_telemetry_send_time_ms = millis() + 10000;
+  while(1){
+  mq2Sensor();
+  tempSensor();
+  delay(5000);
   }
-
-  // MQTT loop must be called to process Device-to-Cloud and Cloud-to-Device.
-  mqtt_loop();
-  delay(500);
-}
-*/
-
-void setup()
-{
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-  establishConnection();
+ 
 }
 
 void loop()
