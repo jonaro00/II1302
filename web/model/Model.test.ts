4390955de8eff1e6433a6f2b90e372d5cd6ce083
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-05-17 17:43:27 +0200

    More model tests

--- web/model/Model.test.ts
+++ web/model/Model.test.ts
@@ -12,10 +12,18 @@ beforeEach(() => {
 })
 
 describe('The Model', () => {
-  it('should notify observers on user change', () => {
+  it('should notify observers on attribute changes', () => {
     const model = new Model()
     model.addObserver(f)
     model.setUsername(testname)
     expect(mock.mock.calls).toHaveLength(1)
+    model.setSensors([])
+    expect(mock.mock.calls).toHaveLength(2)
+    model.setDeviceTelemetry(1, [])
+    expect(mock.mock.calls).toHaveLength(3)
+    model.setDeviceEvents(1, [])
+    expect(mock.mock.calls).toHaveLength(4)
+    model.setDeviceAlarms(1, [])
+    expect(mock.mock.calls).toHaveLength(5)
   })
 })
