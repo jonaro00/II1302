c3f4aa27a4d6f86903ef0c7340cb7b451eef3f31
Bansheebomb <dugiev123@gmail.com>
2022-05-17 15:46:01 +0200

    finished DAO tests

--- web/integration/DAO.test.ts
+++ web/integration/DAO.test.ts
@@ -28,114 +28,101 @@ describe('The DAO', () => {
 
   it('should not let a user register twice', async () => {
     await dao.register({ username: testname, password: testpass })
-    expect(dao.register({ username: testname, password: testpass })).rejects.toThrow(
+    await expect(dao.register({ username: testname, password: testpass })).rejects.toThrow(
       'Failed to register user.',
     )
   })
 
   it('should not accept an incorrect password', async () => {
     await dao.register({ username: testname, password: testpass })
-    expect(dao.login({ username: testname, password: '123456' })).rejects.toThrow(
+    await expect(dao.login({ username: testname, password: '123456' })).rejects.toThrow(
       'Invalid password!',
     )
   })
 
   it('should not accept an incorrect user', async () => {
-    expect(dao.login({ username: 'obamas last  name', password: 'bruh' })).rejects.toThrow(
+    await expect(dao.login({ username: 'obamas last  name', password: 'bruh' })).rejects.toThrow(
       'No user with that name found!',
     )
   })
 
-  it.only('should get a sensor with a matching user id', async () => {
-    await dao.addSensor(testid, { device_azure_name: 'azure name', location: 'sweden' })
-    const arr = await dao.getSensors(testid)
-    var check = false
-    for (const x of arr) {
-      if (x.user_id === testid) {
-        check = true
-      }
-    }
-    expect(arr).not.toBeNull()
-    expect(check).toEqual(true)
+  it('should add a sensor', async () => {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await expect(
+      dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' }),
+    ).resolves.toBeUndefined()
   })
 
-  it('should add a sensor', async () => {
-    await dao.addSensor(testid, { device_azure_name: 'azure name', location: 'sweden' })
-    const arr = await dao.getSensors(testid)
-    var check = false
-    for (const x of arr) {
-      if (x.user_id === testid) {
-        check = true
-      }
-    }
-    expect(arr).not.toBeNull()
-    expect(check).toEqual(true)
+  it('should get a sensor with a matching user id', async () => {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    const arr = await dao.getSensors(id)
+    expect(arr).toHaveLength(1)
+    expect(arr[0].user_id).toEqual(id)
   })
 
   it('should update a sensor', async () => {
-    await dao.addSensor(testid, { device_azure_name: 'azure name', location: 'sweden' })
-    await dao.updateSensor(testid, sensorid, {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    await dao.updateSensor(id, sensorid, {
       device_azure_name: 'azure name',
       location: 'germany',
     })
-    const arr = await dao.getSensors(testid)
-    var check = false
-    for (const x of arr) {
-      if (x.location === 'germany') {
-        check = true
-      }
-    }
-    expect(arr).not.toBeNull()
-    expect(check).toEqual(true)
+    const arr = await dao.getSensors(id)
+    expect(arr).toHaveLength(1)
+    expect(arr[0].user_id).toEqual(id)
+    expect(arr[0].location).toEqual('germany')
   })
 
   it('should delete a sensor', async () => {
-    await dao.addSensor(testid, { device_azure_name: 'azure name', location: 'sweden' })
-    await dao.deleteSensor(testid, sensorid)
-    const arr = await dao.getSensors(testid)
-    expect(arr).toBeNull()
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    let arr = await dao.getSensors(id)
+    expect(arr).toHaveLength(1)
+    await dao.deleteSensor(id, arr[0].id)
+    arr = await dao.getSensors(id)
+    expect(arr).toHaveLength(0)
   })
 
-  it('should get telemetry with a matching sensor id', async () => {
-    const arr = await dao.getTelemetry(
-      testid,
-      sensorid,
-      new Date('January 1, 2022 10:00:00'),
-      new Date('January 1, 2023 10:00:00'),
-      5,
-      100,
-    )
-    var check = false
-    for (const x of arr) {
-      if (x.sensor_id === sensorid) {
-        check = true
-      }
-    }
-    expect(arr).not.toBeNull()
-    expect(check).toEqual(true)
+  it('should not allow different users to delete sensor', async () => {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    const { id: id2 } = await dao.register({ username: 'user2', password: 'password2' })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    let arr = await dao.getSensors(id)
+    expect(arr).toHaveLength(1)
+    await expect(dao.deleteSensor(id2, arr[0].id)).rejects.toThrow('Failed to delete sensor.')
   })
 
   it('should add telemetry with a matching sensor id', async () => {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    await expect(
+      dao.addTelemetry(
+        'azure name',
+        { temp: 5, humidity: 5, lpg: 5, co: 5, smoke: 5 },
+        new Date('January 1, 2022 10:00:01'),
+      ),
+    ).resolves.toBeUndefined()
+  })
+
+  it('should get telemetry with a matching sensor id', async () => {
+    const { id } = await dao.register({ username: testname, password: testpass })
+    await dao.addSensor(id, { device_azure_name: 'azure name', location: 'sweden' })
+    const sensor = (await dao.getSensors(id))[0]
     await dao.addTelemetry(
       'azure name',
       { temp: 5, humidity: 5, lpg: 5, co: 5, smoke: 5 },
       new Date('January 1, 2022 10:00:01'),
     )
     const arr = await dao.getTelemetry(
-      testid,
-      sensorid,
+      id,
+      sensor.id,
       new Date('January 1, 2022 10:00:00'),
       new Date('January 1, 2023 10:00:00'),
       5,
       100,
     )
-    var check = false
-    for (const x of arr) {
-      if (x.co === 5) {
-        check = true
-      }
-    }
-    expect(arr).not.toBeNull()
-    expect(check).toEqual(true)
+    expect(arr).toHaveLength(1)
+    expect(arr[0].humidity).toEqual(5)
   })
 })
